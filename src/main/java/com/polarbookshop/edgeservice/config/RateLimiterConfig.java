1f744c04c397dc5a3932eb01bb6e872bf13d199c
lsh <tmdgns4029@naver.com>
2024-06-29 09:55:02 +0900

    사용자 별 사용률 제한 설정 변경(anonymous -> principal)

--- src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
@@ -3,14 +3,18 @@ package com.polarbookshop.edgeservice.config;
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import reactor.core.publisher.Mono;
+
+
+import java.security.Principal;
 
 @Configuration
 public class RateLimiterConfig {
 
-
+    // 사용자 별 사용률 제한
     @Bean
     public KeyResolver keyResolver(){
-        return exchange -> Mono.just("anonymous");
+        return exchange -> exchange.getPrincipal()
+                .map(Principal::getName)
+                .defaultIfEmpty("anonymous");
     }
 }
